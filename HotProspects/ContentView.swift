cfc745df7b7f32a50af88f46464a75ab0178ea68
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-12 02:28:54 +0530

    Learned tabView

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -9,19 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    let user = User()
+    @State private var selectedTab = 0
     var body: some View {
-        //        VStack {
-        //            EditView().environmentObject(user)
-        //            DisplayView().environmentObject(user)
-        //        }
-        
-        //        Both works exactly same way now we place user in ContentView's environment
-        
-        VStack {
-            EditView()
-            DisplayView()
-        }.environmentObject(user)
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    self.selectedTab = 1
+            }
+            .tabItem {
+                Image(systemName: "star")
+                Text("One")
+            }
+            .tag(0)
+            
+            Text("Tab 2")
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Two")
+            }
+            .tag(1)
+        }
     }
 }
 
@@ -29,23 +36,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
-
-struct EditView: View {
-    @EnvironmentObject var user: User
-    var body: some View {
-        TextField("Name", text: $user.name)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        Text(user.name)
-    }
-}
-
-class User: ObservableObject {
-    @Published var name = "Taylor Swift"
 }
