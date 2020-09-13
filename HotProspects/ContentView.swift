6d8a8252bb6911e00a562282946004a2a3d58fbc
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-13 21:41:40 +0530

    learned about objectWillChange

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var updater = DelayedUpdater()
     
     func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void)  {
         // check the URL is OK, otherwise return with a failure
@@ -36,23 +37,26 @@ struct ContentView: View {
     }
     
     var body: some View {
-        Text("Hello, World!")
-            .onAppear {
-                self.fetchData(from: "https://www.apple.com") { (result) in
-                    switch result {
-                        case .success(let str):
-                            print(str)
-                        case .failure(let error):
-                            switch error {
-                                case .badURL:
-                                    print("Bad URL")
-                                case .requestFailed:
-                                    print("Network problems")
-                                case .unknown:
-                                    print("Unknown error")
+        VStack {
+            Text("The value is => \(updater.value)")
+            Text("Hello, World!")
+                .onAppear {
+                    self.fetchData(from: "https://www.apple.com") { (result) in
+                        switch result {
+                            case .success(let str):
+                                print(str)
+                            case .failure(let error):
+                                switch error {
+                                    case .badURL:
+                                        print("Bad URL")
+                                    case .requestFailed:
+                                        print("Network problems")
+                                    case .unknown:
+                                        print("Unknown error")
+                            }
                         }
                     }
-                }
+            }
         }
     }
 }
@@ -65,4 +69,20 @@ struct ContentView_Previews: PreviewProvider {
 
 enum NetworkError: Error {
     case badURL, requestFailed, unknown
+}
+
+class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+    
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
 }
