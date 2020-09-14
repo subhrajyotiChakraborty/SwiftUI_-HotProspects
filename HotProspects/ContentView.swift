1f0c8b67eb15e3b8707be741d76dc8d43247ce77
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-14 13:21:08 +0530

    about SPN

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,10 +8,22 @@
 
 import SwiftUI
 import UserNotifications
+import SamplePackage
 
 struct ContentView: View {
+    let possibleNumbers = Array(1...60)
+   
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.joined(separator: ", ")
+    }
+    
     var body: some View {
         VStack {
+            
+            Text(results)
+            
             Button("Request Permission") {
                 UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { success, error in
                     if success {
