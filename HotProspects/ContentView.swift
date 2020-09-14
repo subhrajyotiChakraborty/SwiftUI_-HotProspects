f58f92729394281b191f55b5dd3c77542360233c
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-14 12:14:03 +0530

    Learned about contextMenu

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -9,89 +9,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var updater = DelayedUpdater()
-    
-    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void)  {
-        // check the URL is OK, otherwise return with a failure
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.badURL))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            DispatchQueue.main.async {
-                if let data = data {
-                    // success: convert the data to a string and send it back
-                    let stringData = String(decoding: data, as: UTF8.self)
-                    completion(.success(stringData))
-                } else if error != nil {
-                    // any sort of network failure
-                    completion(.failure(.requestFailed))
-                } else {
-                    // this ought not to be possible, yet here we are
-                    completion(.failure(.unknown))
-                }
-            }
-        }.resume()
-        
-    }
+    @State private var backgroundColor = Color.red
     
     var body: some View {
         VStack {
-            Image("example")
-                .interpolation(.none)
-                .resizable()
-                .scaledToFit()
-                .frame(maxWidth: .infinity)
-                .background(Color.black)
-                .edgesIgnoringSafeArea(.all)
-            
-            Text("The value is => \(updater.value)")
-            
             Text("Hello, World!")
-                .onAppear {
-                    self.fetchData(from: "https://www.apple.com") { (result) in
-                        switch result {
-                            case .success(let str):
-                                print(str)
-                            case .failure(let error):
-                                switch error {
-                                    case .badURL:
-                                        print("Bad URL")
-                                    case .requestFailed:
-                                        print("Network problems")
-                                    case .unknown:
-                                        print("Unknown error")
-                            }
-                        }
+                .padding()
+                .background(backgroundColor)
+            
+            Text("Change Color")
+                .padding()
+                .contextMenu {
+                    Button(action: {
+                        self.backgroundColor = .red
+                    }) {
+                        Text("Red")
+                        Image(systemName: "checkmark.circle.fill")
+                            .foregroundColor(.red)
+                    }
+                    
+                    Button(action: {
+                        self.backgroundColor = .green
+                    }) {
+                        Text("Green")
+                    }
+                    
+                    Button(action: {
+                        self.backgroundColor = .blue
+                    }) {
+                        Text("Blue")
                     }
             }
         }
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-enum NetworkError: Error {
-    case badURL, requestFailed, unknown
-}
-
-class DelayedUpdater: ObservableObject {
-    var value = 0 {
-        willSet {
-            objectWillChange.send()
-        }
-    }
-    
-    init() {
-        for i in 1...10 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
-                self.value += 1
-            }
-        }
     }
 }
