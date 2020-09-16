523f828cbbb98be8fe2018db38fb8f9ed3cf83d2
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-16 13:57:14 +0530

    added code scanner and meView's functionality

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ProspectsView: View {
     let filter: FilterType
@@ -32,6 +33,24 @@ struct ProspectsView: View {
     }
     
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
+    
+    func handleScan(result: Result<String, CodeScannerView.ScanError>) {
+        self.isShowingScanner = false
+        switch result {
+            case .success(let code):
+                let details = code.components(separatedBy: "\n")
+                guard details.count == 2 else { return }
+                
+                let person = Prospect()
+                person.name = details[0]
+                person.emailAddress = details[1]
+                self.prospects.people.append(person)
+            case .failure(let error):
+                print("Scanning failed")
+                print(error.localizedDescription)
+        }
+    }
     
     var body: some View {
         NavigationView {
@@ -43,18 +62,23 @@ struct ProspectsView: View {
                         Text(prospect.emailAddress)
                             .foregroundColor(.secondary)
                     }
+                    .contextMenu {
+                        Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted") {
+                            self.prospects.toggle(prospect)
+                        }
+                    }
                 }
             }
-            .navigationBarTitle(title)
-            .navigationBarItems(trailing: Button(action: {
-                let prospect = Prospect()
-                prospect.name = "Subha Chakraborty"
-                prospect.emailAddress = "ss@ss.com"
-                self.prospects.people.append(prospect)
-            }, label: {
-                Image(systemName: "qrcode.viewfinder")
-                Text("Scan")
-            }))
+            .sheet(isPresented: $isShowingScanner, content: {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: self.handleScan)
+            })
+                .navigationBarTitle(title)
+                .navigationBarItems(trailing: Button(action: {
+                    self.isShowingScanner = true
+                }, label: {
+                    Image(systemName: "qrcode.viewfinder")
+                    Text("Scan")
+                }))
         }
     }
 }
