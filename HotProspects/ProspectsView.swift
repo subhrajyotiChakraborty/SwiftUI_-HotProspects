ea7a3207d1bf4820a059624370485d4068cee9e1
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-17 20:06:28 +0530

    added local notification and userdefaults to save the data

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 import CodeScanner
+import UserNotifications
 
 struct ProspectsView: View {
     let filter: FilterType
@@ -45,11 +46,46 @@ struct ProspectsView: View {
                 let person = Prospect()
                 person.name = details[0]
                 person.emailAddress = details[1]
-                self.prospects.people.append(person)
+                self.prospects.add(person)
             case .failure(let error):
                 print("Scanning failed")
                 print(error.localizedDescription)
         }
+    }
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = UNNotificationSound.default
+            
+            var dateComponents = DateComponents()
+            dateComponents.hour = 9
+            //            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            
+            // Testing purpose
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+        
+        center.getNotificationSettings { (settings) in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("Permission denied!")
+                    }
+                }
+            }
+        }
+        
     }
     
     var body: some View {
@@ -65,6 +101,12 @@ struct ProspectsView: View {
                     .contextMenu {
                         Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted") {
                             self.prospects.toggle(prospect)
+                        }
+                        
+                        if !prospect.isContacted {
+                            Button("Remind Me") {
+                                self.addNotification(for: prospect)
+                            }
                         }
                     }
                 }
